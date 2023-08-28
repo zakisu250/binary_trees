49eecc8d0d20f303f49666e2ebcc4b415fe308e3
zakisu250 <zakisu250@gmail.com>
2023-08-28 07:13:36 -0700

    Creating a binary tree parent node with null childrens

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,22 @@
+#include"binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a new binary tree node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: the value to store in the new node
+ *
+ * Return: Pointer to the new node or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t* newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->parent = parent;
+	newNode->n = value;
+	return (newNode);
+}
