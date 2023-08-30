53d51ee9d808a445043ae552a8b3c9b1bb927aff
zakisu250 <zakisu250@gmail.com>
2023-08-30 14:34:02 -0700

    binary tree ancestor

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *right = NULL;
 	binary_tree_t *parent = (node != NULL ? node->parent : NULL);
 
-	if ((parent != NULL) && (parent-> parent != NULL))
+	if ((parent != NULL) && (parent->parent != NULL))
 	{
 		left = parent->parent->left;
 		right = parent->parent->right;
